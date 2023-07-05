3f24562164c1cbd20dd3ff2f39f55260d34de7ac
dukelionstt <53612742+dukelionstt@users.noreply.github.com>
2023-07-05 14:49:26 +0000

    added logic to check and uncheck etc but not quite there for next steps

--- src/app/modal/open-menu/open-menu.component.ts
+++ src/app/modal/open-menu/open-menu.component.ts
@@ -1,26 +1,78 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Component({
   selector: 'app-open-menu',
   templateUrl: './open-menu.component.html',
-  styleUrls: ['./open-menu.component.css']
+  styleUrls: ['./open-menu.component.css'],
 })
-export class OpenMenuComponent implements OnInit {
+export class OpenMenuComponent implements OnInit, AfterViewInit {
+  @Input()
+  newPageEntry!: any;
 
   @Input()
-  newPageEntry?: any;
+  pageNameList!: Map<string, string>;
 
   @Input()
-  pageNameList?: Map<string, string>;
+  isAllPagesOpen!: boolean;
 
-  @Input()
-  isAllPagesOpen?: boolean; 
-
+  pagesToOpen!: string[];
   dateToday = new Date().toLocaleDateString();
 
-  constructor() { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
+    this.pagesToOpen = [];
   }
 
+  ngAfterViewInit(): void {
+    console.debug('running AfterViewInit');
+    this.pageNameList.forEach((value: string, key: string) =>
+      this.checkPage(key, true)
+    );
+  }
+
+  checkPageToBeOpened(id: string) {
+    if (!this.pageNameList.has(id)) {
+      this.checkPage(id, true);
+      this.pagesToOpen.push(id);
+    } else {
+      this.checkPage(id, false);
+      let temp: string[] = [];
+      this.pagesToOpen.forEach((val) => {
+        if (val != id) temp.push(val);
+      });
+      this.pagesToOpen = temp;
+    }
+  }
+
+  private checkPage(id: string, state: boolean) {
+    console.debug(
+      `running check page with params: id as ${id} and state as ${state}`
+    );
+    let uncheckboxId = `#uncheckedPageOption${id}`;
+    let checkboxId = `#checkedPageOption${id}`;
+    console.debug(
+      `unchecked id: ${uncheckboxId} and checked id: ${checkboxId}`
+    );
+    let uncheckbox = this.elementRef.nativeElement.querySelector(uncheckboxId);
+    let checkbox = this.elementRef.nativeElement.querySelector(checkboxId);
+    console.debug(uncheckbox);
+    console.debug(checkbox);
+    if (state) {
+      console.debug('running check');
+      this.renderer.removeStyle(checkbox, 'display');
+      this.renderer.setStyle(uncheckbox, 'display', 'none');
+    } else {
+      console.debug('running uncheck');
+      this.renderer.setStyle(checkbox, 'display', 'none');
+      this.renderer.removeStyle(uncheckbox, 'display');
+    }
+  }
 }
